3743747bac857e21c7916f2c774585fadaaf5417
RubiRamos <rubi.ramos2244@uppuebla.edu.mx>
2024-03-07 17:40:37 -0600

    class Usuario

---
+++ src/paquetes5B/sesion/Usuario.java
@@ -0,0 +1,27 @@
+package paquetes5B.sesion;
+
+public class Usuarios {
+    private String usuarios;
+    private String contrasennia;
+    private String nombre;
+
+    public String  getUsuarios(){
+        return usuarios;
+    }
+    public void setUsuarios(String usuario){
+        this.usuarios=usuarios;
+    }
+    public String getContrasennia(){
+        return contrasennia;
+    }
+    public void setContrasennia(String contrasennia){
+        this.contrasennia=contrasennia;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+}
+
