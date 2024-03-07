cd3a1637b7f778dacfeddf16c17c46166d343b3c
JimenaPanohayaArenas <jimena.panohaya1816@gmail.com>
2024-03-07 17:52:34 -0600

    Paquete estudiantes

--- src/paquetes5B/estudiantes/Estudiante.java
+++ src/paquetes5B/estudiantes/Estudiante.java
@@ -1,4 +1,34 @@
 package paquetes5B.estudiantes;
 
+
+
 public class Estudiante {
+
+    private String nombre;
+    private int matricula;
+    private String carrear;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCarrear() {
+        return carrear;
+    }
+
+    public void setCarrear(String carrear) {
+        this.carrear = carrear;
+    }
 }
