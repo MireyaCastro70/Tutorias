089b60915050f8857d9fa3f22084111ba4c3252a
israelxicale <amil.xicale2220@uppuebla.edu.mx>
2024-03-07 18:19:23 -0600

    class_ControlUsuarios_elmerobueno

--- src/paquetes5B/sesion/ControlUsuario.java
+++ src/paquetes5B/sesion/ControlUsuario.java
@@ -1,11 +1,10 @@
 package paquetes5B.sesion;
-
 public class ControlUsuarios {
 
     private Usuarios[] usuarios;
 
     public ControlUsuarios() {
-        this.usuarios = new Usuarios[];
+        this.usuarios = new Usuarios[10];
     }
 
     public void addUsuario(String usuario, String pass, String nombre) {
